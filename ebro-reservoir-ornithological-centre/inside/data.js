ed4be13babc8cdfb942392db22c78f3e54942689
kyradai <ayoub.device0@gmail.com>
2026-08-19 18:00:03 +0200

    addfolder

---
+++ ebro-reservoir-ornithological-centre/inside/data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pxl_20260819_093503123photosphere",
+      "name": "PXL_20260819_093503123.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6951630580209525,
+          "pitch": 0.3802824841378438,
+          "rotation": 0,
+          "target": "1-pxl_20260819_093709897photosphere"
+        },
+        {
+          "yaw": -2.3706505013114363,
+          "pitch": 0.3059101382440641,
+          "rotation": 0,
+          "target": "2-pxl_20260819_101943125photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pxl_20260819_093709897photosphere",
+      "name": "PXL_20260819_093709897.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2266939809285464,
+          "pitch": 0.8565110840061969,
+          "rotation": 0,
+          "target": "0-pxl_20260819_093503123photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pxl_20260819_101943125photosphere",
+      "name": "PXL_20260819_101943125.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7934173430535516,
+          "pitch": 0.2716354857311334,
+          "rotation": 0,
+          "target": "3-pxl_20260819_102152882photosphere"
+        },
+        {
+          "yaw": -2.191346343572974,
+          "pitch": 0.2413740507085933,
+          "rotation": 0,
+          "target": "4-pxl_20260819_101325018photosphere"
+        },
+        {
+          "yaw": 1.3104068013860637,
+          "pitch": 0.07141375852796372,
+          "rotation": 0,
+          "target": "0-pxl_20260819_093503123photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pxl_20260819_102152882photosphere",
+      "name": "PXL_20260819_102152882.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.987008598927803,
+          "pitch": 0.3229825943771054,
+          "rotation": 0,
+          "target": "2-pxl_20260819_101943125photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-pxl_20260819_101325018photosphere",
+      "name": "PXL_20260819_101325018.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.030400537138612904,
+          "pitch": 0.41077269826917906,
+          "rotation": 0,
+          "target": "2-pxl_20260819_101943125photosphere"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Ebro Reservoir Ornithological Centre / Inside",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
